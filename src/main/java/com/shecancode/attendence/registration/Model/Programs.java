fc6bb9cf89743b6c4270c730a6eb5486b597ec3f
mbandainnocent <innocent@igirerwanda.org>
2026-02-02 12:54:25 -0500

    04-pushing-the-changes

--- src/main/java/com/shecancode/attendence/registration/Model/Programs.java
+++ src/main/java/com/shecancode/attendence/registration/Model/Programs.java
@@ -2,16 +2,15 @@ package com.shecancode.attendence.registration.Model;
 
 import jakarta.persistence.*;
 import lombok.*;
-import org.springframework.web.service.annotation.GetExchange;
 
 import java.util.UUID;
 
 @Builder
 @Getter
 @Setter
-@Entity
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
 @Table(name = "PROGRAM")
 
 public class Programs {
@@ -25,5 +24,4 @@ public class Programs {
 
     @Column(name = "program_running_period", nullable = false)
     private Integer requiredProgramDuration;
-
 }
