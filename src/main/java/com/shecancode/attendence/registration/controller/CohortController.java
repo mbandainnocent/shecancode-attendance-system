b9cb177bb1cc947b15d3094308f6164bd211dfef
mbandainnocent <innocent@igirerwanda.org>
2026-06-05 15:58:06 -0400

    Update cohort controller and application config

--- src/main/java/com/shecancode/attendence/registration/controller/CohortController.java
+++ src/main/java/com/shecancode/attendence/registration/controller/CohortController.java
@@ -1,9 +1,7 @@
 package com.shecancode.attendence.registration.controller;
 
-<<<<<<< HEAD
-=======
 import com.shecancode.attendence.registration.Model.Cohort;
->>>>>>> 9327538160dac42747dd38ffc4bfe9034b75a9e4
+
 import com.shecancode.attendence.registration.dao.CohortRequestDao;
 import com.shecancode.attendence.registration.dao.CohortResponseDao;
 import com.shecancode.attendence.registration.service.CohortService;
@@ -11,19 +9,18 @@ import com.shecancode.attendence.registration.util.LoggingUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-<<<<<<< HEAD
+
 import org.springframework.security.access.prepost.PreAuthorize;
-=======
->>>>>>> 9327538160dac42747dd38ffc4bfe9034b75a9e4
+
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
-<<<<<<< HEAD
+
 @RequestMapping("/api/v1/cohorts")
-=======
+
 @RequestMapping("/api/v1/cohort")
->>>>>>> 9327538160dac42747dd38ffc4bfe9034b75a9e4
+
 public class CohortController {
     private final  CohortService cohortService;
 
@@ -41,9 +38,8 @@ public class CohortController {
 
     @PostMapping
     @PreAuthorize("hasRole('ADMIN')")
-=======
+
     @PostMapping
->>>>>>> 9327538160dac42747dd38ffc4bfe9034b75a9e4
     protected ResponseEntity<CohortResponseDao> createCohort(@RequestBody CohortRequestDao cohortRequestDao){
         log.info("Received cohort request: {}", LoggingUtils.sanitizeForLogging(cohortRequestDao.getCohortNumber()));
 
